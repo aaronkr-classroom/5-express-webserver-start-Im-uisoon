55471cbd00c78f465aafbb5708469924c8765f13
Doran <34602135+Im-uisoon@users.noreply.github.com>
2024-04-17 05:06:22 +0000

    11

--- lesson-11/controllers/homeController.js
+++ lesson-11/controllers/homeController.js
@@ -4,14 +4,50 @@
 /**
  * Listing 10.2
  */
-exports.respondWithName = (req, res) => {
+exports.respondWithName1 = (req, res) => {
     res.render('index');
 }
 
 /**
  * Listing 10.3
  */
-exports.respondWithName2 = (req, res) => {
-    let paramsName = req.params.myName;
-    res.render('index', { name: paramsName });
+exports.getHome = (req, res) => { res.render('index', {
+    name: "Sarah",
+    people: {
+        usa: {
+            einstein: "Alber Einstein",
+            musk: "Elon Musk",
+            jobs: "Steve Jobs"
+        },
+        korea: {
+            psy: "싸이다!",
+            kim: "김예수"
+        },
+        mexico: {
+            jesus: "Jesus Alvarez"
+        }
+    }
+}); } // "/" index
+// exports.getContact = (req, res) => {} // "/Contact" Get method
+// exports.postContact = (req, res) => {} // "/Contact" Post method
+
+exports.respondWithName = (req, res) => {
+    let paramsName = req.params.myName;  // 라우트 매개변수 /name/:myname
+    res.render('index', { 
+        name: paramsName ,
+        people: {
+            usa: {
+                einstein: "Alber Einstein",
+                musk: "Elon Musk",
+                jobs: "Steve Jobs"
+            },
+            korea: {
+                psy: "싸이다!",
+                kim: "김예수"
+            },
+            mexico: {
+                jesus: "Jesus Alvarez"
+            }
+        }
+    });
 };
